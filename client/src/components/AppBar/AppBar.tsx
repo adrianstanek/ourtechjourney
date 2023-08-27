794ff19d74c78cb6caebea8ed0018b629d408192
Adrian Stanek <stanek@bitsinmotion.de>
2023-08-27 09:12:59 +0200

    add: Story Add Button uui
    Took 6 minutes

--- client/src/components/AppBar/AppBar.tsx
+++ client/src/components/AppBar/AppBar.tsx
@@ -5,11 +5,29 @@ export interface IAppBar {}
 export const AppBar: React.FC<IAppBar> = () => {
     return (
         <>
-            <section className="relative z-20 flex h-16 w-full flex-row items-center justify-center gap-1">
-                <button className="relative -top-2 mx-auto flex aspect-[1/1] h-16 items-center justify-center rounded-full bg-primary-dark text-secondary ring-2 ring-offset-2 ring-offset-white focus:outline-0 focus:ring">
+            <section className="relative z-20 flex h-16 w-full flex-row items-center justify-center gap-4">
+                <button
+                    id="newMomentButton"
+                    role="button"
+                    aria-label="Create new Moment"
+                    className="relative -top-2  flex aspect-[1/1] h-16 items-center justify-center rounded-full bg-primary-dark text-secondary ring-2 ring-offset-2 ring-offset-white focus:outline-0 focus:ring"
+                >
                     <img
                         src="/assets/icons/noun-moment-2700515-E4F9F5.svg"
                         className="h-12"
+                        alt="Create a Moment"
+                    />
+                </button>
+
+                <button
+                    id="newStoryButton"
+                    role="button"
+                    aria-label="Start new Story"
+                    className="absolute right-2 top-2  flex aspect-[1/1] h-12 items-center justify-center rounded-full bg-primary-dark text-secondary ring-2 ring-offset-2 ring-offset-white focus:outline-0 focus:ring"
+                >
+                    <img
+                        src="/assets/icons/noun-story-3203471-E4F9F5.svg"
+                        className="relative -top-[1px] h-7"
                         alt="Create a Moment"
                     />
                 </button>
