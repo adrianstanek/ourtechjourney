c888a3dced719567832a0c5e01560a2d0acbcea5
Adrian Stanek <stanek@bitsinmotion.de>
2023-09-04 17:44:39 +0200

    add: Recoillize
    Took 9 minutes

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -9,8 +9,26 @@ import { MapProvider } from 'react-map-gl';
 import { SWUpdater } from '../src/components/pwa/SWUpdater';
 import { useStorage } from '../src/hooks/storage/useStorage';
 import { StoryListModal } from '../src/components/Stories/StoryListModal';
+import dynamic from 'next/dynamic';
 
 const MyApp = ({ Component, pageProps }: AppProps): unknown => {
+    const [root, setRoot] = useState(null);
+
+    const RecoilizeDebugger = dynamic(
+        () => {
+            return import('recoilize');
+        },
+        { ssr: false }
+    );
+
+    useEffect(() => {
+        if (typeof window.document !== 'undefined') {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            setRoot(document.getElementById('__next'));
+        }
+    }, [root]);
+
     const queryClient = new QueryClient({
         defaultOptions: {
             queries: {
@@ -32,6 +50,7 @@ const MyApp = ({ Component, pageProps }: AppProps): unknown => {
         <QueryClientProvider client={queryClient}>
             <RecoilRoot>
                 <MapProvider>
+                    <RecoilizeDebugger root={root} />
                     <Component {...pageProps} />
                     <SWUpdater />
                     {/*/!*<ReactQueryDevtools />*!/*/}
