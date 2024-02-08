7a65c9ed40d8e3ff9bc276d437ea683bcb76e30d
Denis Čahuk <denis.cahuk@gmail.com>
2024-02-08 17:40:33 +0100

    chore(99bottles): Testing plan

--- client/99bottles/99bottles.spec.ts
+++ client/99bottles/99bottles.spec.ts
@@ -1,3 +1,17 @@
 describe('99 Bottles of Beer', () => {
-
+    /*
+     * Do we care about plurality? Human readability? YES
+     * I'm going to loop 99-ish times. When I'm done I have a final statement
+     *
+     * 1. The last VERSE of the song is always:
+     *  "No more bottles of beer on the wall, no more bottles of beer.
+     *   Go to the store and buy some more, 99 bottles of beer on the wall."
+     * 2. The song has 99 verses plus the final one.
+     * 3. The song starts at the verse containing 99 bottles
+     * 4. The song goes on by decrementing the number of bottles in each verse
+     * 5. If we have more than one bottle, it is 'bottles', instead of 'bottle'.
+     * 6. The basic verse structure is: "X bottles of beer on the wall, X bottles of beer
+     *                                   Take one down and pass it around, X-1 bottle(s) of beer on the wall"
+     * 7. A number next the word bottle, possibly with an s (ie. bottles) for plural meanings.
+     */
 });
