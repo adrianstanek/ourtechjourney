24f9a172c87ee0f3e2c8251828179d49691fbad2
Denis Čahuk <denis.cahuk@gmail.com>
2024-02-11 13:29:23 +0100

    doc(99bottles-solo): Testing plan

---
+++ client/99bottles-solo/99bottles.spec.ts
@@ -0,0 +1,15 @@
+describe('99 Bottles of Beer lyrics - song', () => {
+    it.skip('is structured with a repeating VERSE form', () => {});
+    it.skip('has VERSES with two SENTENCES');
+    it.skip('the 1st SENTENCE has the BOTTLES SECTION and its RHYME');
+    it.skip('the 2nd SENTENCE has the ACTION and NEW BOTTLES SECTION');
+    it.skip("refers to a specific number of bottles in each VERSE's 1st SENTENCE", () => {});
+    it.skip('decrements the number of bottles in the 2nd SENTENCE', () => {});
+    it.skip("repeats the previous verse's 2nd SENTENCE in the following VERSE's 1st SENTENCE", () => {});
+    it.skip('styles the zero bottle ACTION with a special bridge section');
+    it.skip('styles the zero bottle BOTTLES SECTION as text: "no more bottles"');
+    it.skip('all non-zero VERSES decrement the number of bottles in the 2nd SENTENCE');
+    it.skip('the last VERSE with zero bottles finishes with 99 bottles in its 2nd sentence.');
+    it.skip('DETAIL: each SENTENCE starts uppercase');
+    it.skip('DETAIL: VERSES are separated by an additional newline');
+});
