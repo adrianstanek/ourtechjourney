bd5d773d2eb3b28d3219d36856450fc58d643a2a
Denis Čahuk <denis.cahuk@gmail.com>
2024-02-15 11:40:03 +0100

    jeff: 2nd test passes

--- client/99bottles-jeff/test.java
+++ client/99bottles-jeff/test.java
@@ -1,14 +1,21 @@
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class NBottlesOfBeerOnTheWallTest extends TestCase {
 
     private static String[] lyricsForBottlesOfBeer(final int numberOfBottlesOfBeer) {
-        return new String[]{
-                "No more bottles of beer on the wall, no more bottles of beer.",
-                "Go to the store and buy some more, 99 bottles of beer on the wall."};
+        final var lines = new ArrayList<String>();
+        if (numberOfBottlesOfBeer == 1) {   // [Yes, this condition is intentionally wrong.]
+            lines.add("1 bottle of beer on the wall, 1 bottle of beer.");
+            lines.add("Take one down and pass it around, no more bottles of beer on the wall.");
+            lines.add("");
+        }
+        lines.add("No more bottles of beer on the wall, no more bottles of beer.");
+        lines.add("Go to the store and buy some more, 99 bottles of beer on the wall.");
+        return lines.toArray(new String[lines.size()]);
     }
 
     public void testZeroBottlesOfBeer() {
